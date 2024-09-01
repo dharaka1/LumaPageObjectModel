badc9256ebd8b279b805185cd240ac0caa8791df
91812 <91812@DharakaSundar>
2024-09-01 12:27:28 +0530

    Hi

--- src/main/java/BasePack/BaseClass.java
+++ src/main/java/BasePack/BaseClass.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.Set;
 
 import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
@@ -15,17 +16,43 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.devtools.idealized.Javascript;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class BaseClass {
 
 	public static WebDriver driver;
 	
+	
+	public static void geturl(String url) {
+		driver.get(url);
+	}
 	public static void maximize() {
 		driver.manage().window().maximize();
 	}
 
-	public static void implicityWait() throws InterruptedException {
-		Thread.sleep(2000);
+	public static void toMaximize() {
+		driver.manage().window().maximize();
+	}
+
+	public static void implicitWait() {
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5000));
+	}
+
+	public static void jsClick(WebElement element) {
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("arguments[0].click() ;", element);
+	}
+
+	public static void jsScroll(By scroll) {
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("arguments[0].scrollIntoView();", scroll);
+	}
+
+	public static void explicitWait(WebElement locator) {
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(50));
+		wait.until(ExpectedConditions.elementToBeClickable(locator));
+
 	}
 
 	public static void Url(String Url) {
